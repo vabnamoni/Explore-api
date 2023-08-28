e1e79c13dd0f34a0e3e1f78dd5a8e84c5710e9a5
vabnamoni <vabnamoni11@gmail.com>
2023-08-28 20:02:25 +0800

    added final

--- js/index.js
+++ js/index.js
@@ -5,7 +5,11 @@ function loadUsers2(){
     
 }
 function displayUser2(data){
+    const ul=document.getElementById('users-list');
     for (const user of data){
         console.log(user.name);
+        const li =document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
     }
 }
